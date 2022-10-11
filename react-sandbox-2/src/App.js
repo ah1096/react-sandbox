7074079faadeb9befff86a143041d57dd3c80693
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-10-11 18:43:31 +0000

    changed css, added cat icon, completed Profile()

--- react-sandbox-2/src/App.js
+++ react-sandbox-2/src/App.js
@@ -1,19 +1,52 @@
 import './App.css';
+import cat from './cat-icon.png'; //I can import images into my HTML page using React
 
+//I can create and nest elements
 function MyButton() {
   return (
-    <button> I'm a button.</button>
+    <button>click me</button>
   );
 }
 
+//I can add classNames to add CSS to elements
 export default function MyApp(){
   return (
     <div>
-      <h1>Welcome to my app</h1>
+      <h1 className="header">insert title here</h1>
+      <p className="subheader">this app was coded using React</p>
       <MyButton />
+      <Profile />
     </div>
   )
 }
+
+//I can display data using JSX and curly braces
+const user = {
+  name: 'Placeholder Cat',
+  imageUrl: cat,
+  imageSize: 90,
+};
+
+export function Profile() {
+  return (
+    <>
+      <h1 className="header">{user.name}</h1>  
+      <img
+          className="catIcon"
+          src={user.imageUrl}
+          alt={'Image of ' + user.name}
+          style={{
+            width: user.imageSize, //can use *2 here to double width; able to manipulate w/ math
+            height: user.imageSize
+          }}
+    />
+    </>
+  );
+}
+
+//I can render conditionals VVVVVVV
+
+
 
 
 
