7b64a4fa4577d5bd69ee12cff88a80499d6c66ce
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-10-11 20:22:14 +0000

    two buttons now generate count independent of each

--- react-sandbox-2/src/App.js
+++ react-sandbox-2/src/App.js
@@ -1,20 +1,34 @@
 import './App.css';
 import cat from './cat-icon.png'; //I can import images into my HTML page using React
+import { useState } from 'react';
 
 //I can create and nest elements
+//I can declare an event handler
 function MyButton() {
+const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
-    <button>click me</button>
+    <button onClick={handleClick}>
+      clicked {count} times
+      </button>
   );
 }
+
 
 //I can add classNames to add CSS to elements
 export default function MyApp(){
   return (
     <div>
-      <h1 className="header">insert title here</h1>
-      <p className="subheader">this app was coded using React</p>
-      <MyButton />
+      <h1 className="mainheader">insert title here</h1>
+      <p className="subheader">this app was coded using React. here are some things I learned about from the React Quick Start tutorial:</p>
+        <MakeAList />
+      <h1 className="header">click these buttons</h1>
+        <MyButton />
+        <MyButton />
       <Profile />
     </div>
   )
@@ -22,7 +36,7 @@ export default function MyApp(){
 
 //I can display data using JSX and curly braces
 const user = {
-  name: 'Placeholder Cat',
+  name: 'placeholder cat',
   imageUrl: cat,
   imageSize: 90,
 };
@@ -45,6 +59,44 @@ export function Profile() {
 }
 
 //I can render conditionals VVVVVVV
+//let content;
+//if (isLoggedIn) {
+//  content = <AdminPanel />;
+//} else {
+//  content = <LoginForm />;
+//}
+//return (
+//  <div>
+//    {content}
+//  </div>
+//)
+
+
+//I can render a list
+function MakeAList() {
+
+  const things = [
+    {title: 'creating and nesting components', isLast: false, id: 1},
+    {title: 'adding classes and styles', isLast: false, id: 2},
+    {title: 'displaying data', isLast: true, id: 3}
+  ];
+  
+  const listItems = things.map(thing =>
+      <li key={thing.id}
+          style={{
+            color: thing.isLast ?  'white' : 'rgb(246, 123, 143)'
+          }}>
+        {thing.title}
+      </li>);
+  
+  return (
+    <ul className="list">{listItems}</ul>
+  );
+  }
+
+
+
+
 
 
 
